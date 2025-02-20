3dae6bf98c60586bbaf03107ac9785070366ce8c
sio2000 <karapanosnikos9@gmail.com>
2025-02-20 10:58:18 -0800

    Fix build configuration

--- pols/vite.config.js
+++ pols/vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 }) 
